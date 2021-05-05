3d22b482cda47413f3b7d14be7ecddc7cad42ec1
Hannahadora <hejimofor141@uniport.edu.ng>
2021-05-05 15:08:54 +0100

    done

---
+++ app.js
@@ -0,0 +1,118 @@
+// Student Constructor
+function Student(firstName, lastName, age, phone, department, time) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.phone = phone;
+    this.department = department;
+    this.time = time;
+}
+
+// UI Constructor
+function UI() {}
+
+UI.prototype.addStudentToList = function(student) {
+    const list = document.getElementById('bigList');
+    // Create tr element
+    const row = document.createElement('tr');
+    // Insert cols
+    row.innerHTML =`
+       <td>${student.firstName}</td>
+       <td>${student.lastName}</td>
+       <td>${student.age}</td>
+       <td>${student.phone}</td>
+       <td>${student.department}</td>
+       <td>${student.time}</td>
+       <td><a href="#" class="delete">X</a></td>
+       `;
+
+    list.appendChild(row);
+}
+
+// Show Alert 
+UI.prototype.showAlert = function(message, className) {
+    // Create Div
+    const div = document.createElement('div');
+    // Add Classes
+    div.className = `alert ${className}`;
+    // Add Text
+    div.appendChild(document.createTextNode(message));
+    // Get Parent
+    const container = document.querySelector('.container');
+     // Get Form
+    const form = document.querySelector('#bigForm');
+    // Insert alert
+    container.insertBefore(div, form);
+
+    // Timeout after 3 sec
+    setTimeout(function(){
+         document.querySelector('.alert').remove();
+        }, 3000);
+    }
+
+    // Delete Student
+    UI.prototype.deleteStudent = function(target) {
+        if(target.className === 'delete') {
+            target.parentElement.parentElement.remove();
+        }
+    }
+
+
+// Clear Fields
+UI.prototype.clearFields = function() {
+    document.getElementById('firstName').value = '';
+    document.getElementById('lastName').value = '';
+    document.getElementById('age').value = '';
+    document.getElementById('phone').value = '';
+    document.getElementById('department').value = '';
+    document.getElementById('time').value = ''
+}
+
+// Event Listeners for add book
+document.getElementById('bigForm').addEventListener('submit', function(e) {
+//    Get form values
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const age = document.getElementById('age').value;
+    const phone = document.getElementById('phone').value;
+    const department = document.getElementById('department').value;
+    const time = document.getElementById('time').value
+
+    //    Instantiate student
+    const student = new Student(firstName, lastName, age, phone, department, time);
+
+    // Instantiate UI
+    const ui = new UI();
+   
+    // Validate
+    if(firstName === '' || lastName === '' || age === '' || phone === '' || department === '' || time === '') {
+    //    Error text
+        ui.showAlert('Please fill in all fields', 'error');
+    } else {
+        // Add student to list
+        ui.addStudentToList(student);
+
+        //    Show success
+        ui.showAlert('Registration complete', 'success')
+
+        // Clear Fields
+        ui.clearFields();
+    }
+    
+    e.preventDefault();
+});
+
+// Event Listener For Delete
+document.getElementById('bigList').addEventListener('click', function(e) {
+
+    // Instantiate UI
+    const ui = new UI();
+
+    // Delete student
+    ui.deleteStudent(e.target);
+
+    // Show Alert
+    ui.showAlert('Profile Deleted', 'success')
+
+    e.preventDefault();
+})
